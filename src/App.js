873b4a1c893d04e279a078f5cf033fc0a452b375
PinYi Lee <pinyilee@PinYideMacBook-Pro.local>
2023-12-11 15:53:16 +0800

    part3 套用react Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const GuessNumberGame = () => {
+  // 使用 useState 定義狀態
+  const [answer, setAnswer] = useState(Math.floor(Math.random() * 100) + 1);
+  const [userInput, setUserInput] = useState("");
+  const [message, setMessage] = useState("猜一個介於 1 到 100 之間的數字：");
+
+  // 使用 useEffect 來模擬 componentDidMount
+  useEffect(() => {
+    const guessField = document.getElementById("guessField");
+
+    // 使用 DOM Element 操作
+    guessField.addEventListener("input", handleInputChange);
+
+    // 返回清理函式以模擬 componentWillUnmount
+    return () => {
+      guessField.removeEventListener("input", handleInputChange);
+    };
+  }, []); // 空依賴表示僅在初次渲染時執行
+
+  // 使用 DOM Element 操作的函式
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  // 處理猜測的函式
+  const handleGuess = () => {
+    const userGuess = parseInt(userInput, 10);
+
+    if (isNaN(userGuess)) {
+      setMessage("請輸入有效的數字！");
+    } else {
+      if (userGuess === answer) {
+        setMessage("恭喜你，猜對了！");
+      } else if (userGuess < answer) {
+        setMessage("猜高一點！");
+      } else {
+        setMessage("猜低一點！");
+      }
+    }
+  };
+
   return (
     <div>
       <h1>猜數字遊戲</h1>
-      <p>猜一個介於 1 到 100 之間的數字：</p>
-      <input type="text" id="guessField" />
-      <button>猜！</button>
-      <p className="message"></p>
+      <p>{message}</p>
+      <input type="text" id="guessField" value={userInput} />
+      <button onClick={handleGuess}>猜！</button>
     </div>
   );
 };
